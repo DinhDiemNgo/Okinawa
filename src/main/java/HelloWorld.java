17841234628709d8dae993fced55941d14b0afd6
銘苅佳奈 <78415755+KanaMekaru@users.noreply.github.com>
2023-11-21 21:49:16 +0900

    I modified something

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String name = "Nguyen";
-        System.out.println(name+" is not Japanese,but Vietnamese");
+        String lastName = "Nguyen";
+        String firstName = "Jazz";
+        System.out.println(lastName+" is not Japanese,but Vietnamese "+firstName);
     }
 }
