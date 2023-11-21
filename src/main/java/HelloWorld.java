4d29ab1f870fe5096b1138a6066a9788bfa0e575
DONG ZHENGBO <chunghwa1997@gmail.com>
2023-11-21 21:44:39 +0900

    I added a Java Class File.

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Nguyen";
+        System.out.println(name+" is not Japanese,but Vietnamese");
+    }
+}
